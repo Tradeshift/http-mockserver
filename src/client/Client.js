de6fd711bb07e0f23d2d477892eb62026e3924e1
Søren Louv-Jansen <slo@tradeshift.com>
2016-12-21 13:27:54 +0100

    Add client to mockServer

---
+++ src/client/Client.js
@@ -0,0 +1,103 @@
+const Q = require('q');
+const { format } = require('url');
+const request = Q.denodeify(require('request'));
+
+class MockClient {
+
+	constructor (port, serverHost) {
+		this.port = port;
+		this.serverHost = serverHost;
+	}
+
+	isListening () {
+		return this.getListener(this.port)
+			.then(response => response.statusCode === 200);
+	}
+
+	getListener () {
+		return this.req({
+			uri: '/listener/' + this.port,
+			method: 'GET'
+		});
+	}
+
+	listen () {
+		return this.req({
+			uri: '/listener/' + this.port,
+			method: 'POST'
+		});
+	}
+
+	getRequests () {
+		return this.req({
+			uri: '/requests/' + this.port,
+			method: 'GET'
+		})
+		.then(res => JSON.parse(res.body));
+	}
+
+	waitForRequest (predicate, count = 1, delay = 500) {
+		return this.getRequests()
+			.then(requests => requests.filter(predicate))
+			.then(requests => {
+				if (requests.length === count) {
+					return requests;
+				} else {
+					return Q.delay(delay).then(() => this.waitForRequest(predicate, count, delay));
+				}
+			});
+	}
+
+	removeListener () {
+		return this.req({
+			uri: '/listener/' + this.port,
+			method: 'DELETE'
+		});
+	}
+
+	clear () {
+		return this.req({
+			uri: '/clear',
+			method: 'DELETE'
+		});
+	}
+
+	allowAll () {
+		return this.addRoute({
+			uri: '*',
+			method: 'GET',
+			response: {}
+		});
+	}
+
+	addRoute (options) {
+		return this.req({
+			uri: '/listener/' + this.port,
+			method: 'POST',
+			json: options
+		});
+	}
+
+	addRoutes (...routes) {
+		return Q.all(routes.map(options => this.addRoute(options)));
+	}
+
+	sendData (options) {
+		return this.req({
+			uri: '/listener/' + this.port + '/chunk',
+			method: 'POST',
+			json: options
+		});
+	}
+
+	req (options) {
+		options.baseUrl = format({
+			protocol: 'http',
+			host: this.serverHost
+		});
+
+		return request(options).spread((response, error) => response);
+	}
+}
+
+module.exports = MockClient;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const app = express();
-const server = http.Server(app);
-app.use(bodyParser.json());
-const controller = require('./controller');
-
-app.get('/listener/:port', controller.getListener);
-app.post('/listener/:port', controller.addListenerOrRoute);
-app.delete('/listener/:port', controller.removeListener);
-
-app.post('/listener/:port/chunk', controller.sendChunk);
-
-app.get('/listener', controller.getListeners);
-app.delete('/clear', controller.clear);
-
-app.get('/requests/:port?', controller.getRequests);
-
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(400).send(err.message);
-});
-
-server.listen(3000);
-console.log('Running on http://localhost:3000');
+module.exports = {
+	MockClient: require('./client/Client')
+};
