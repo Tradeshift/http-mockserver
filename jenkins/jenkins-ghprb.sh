6e25a66c21f386f6c34b1641dfb0f5fc09cb8804
Alexander Shilokhvostov <ash@tradeshift.com>
2020-02-11 17:24:56 +0700

    DES-1206 Switch docker registry to eu.gcr.io
    docker.tradeshift.net is going to be depricated. Switch docker registry to eu.gcr.io

--- jenkins/jenkins-ghprb.sh
+++ jenkins/jenkins-ghprb.sh
@@ -4,7 +4,7 @@
 # Job: https://ci.ts.sv/view/Collaboration/job/http-mockserver-ghprb
 
 set -e
-IMAGE_NAME=docker.tradeshift.net/http-mockserver:$(git rev-parse HEAD)
+IMAGE_NAME=eu.gcr.io/tradeshift-base/http-mockserver:$(git rev-parse HEAD)
 docker build -t $IMAGE_NAME .
 
 docker run $IMAGE_NAME npm test
